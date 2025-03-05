c3f3569e979d2eb49b0352fc4dd26b0dfc7a13d8
AzmeerSohail <azmeersohail013@gmail.com>
2025-03-05 16:30:23 +0500

    All changes made

--- tests/test.py
+++ tests/test.py
@@ -3,11 +3,13 @@ import os
 import unittest
 import json
 
-# Add the 'src' directory to sys.path
+# Ensure 'src' directory is in the Python path before importing app.py
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src"))
+)
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+from app import app, validate_input  # noqa: E402
 
-from app import app, validate_input
 
 class TestHousePriceAPI(unittest.TestCase):
     """
